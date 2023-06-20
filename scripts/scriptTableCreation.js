8c4b805f611f6f8467e2d385861c0740a237871d
KeuraChan <109036590+KeuraChan@users.noreply.github.com>
2023-06-20 15:02:20 -0300

    Correção de erro
    Corrigindo erro no script para criação de mesas na tela principal;
    O script dee criação de mesas havia perdido o sinal de ` que fazia a criação do estilo de uma nova mesa

--- scripts/scriptTableCreation.js
+++ scripts/scriptTableCreation.js
@@ -6,22 +6,22 @@ document.getElementById('createNewTable').addEventListener('click', () => {
     novaMesa.className = 'cardMesa'; // Define a classe "cardMesa" para a nova mesa.
 
     // Define o conteúdo HTML da nova mesa usando a propriedade innerHTML.
-    novaMesa.innerHTML = 
-    <div onclick="window.location.href='table.html'">
-      <h3 class="cardMesa-titulo">Nome da Mesa</h3>
-      <div class="cardMesa-header">
-        <img src="../media/logo_mesa.png" alt="Imagem do Card" class="cardMesa-img" />
-        <div class="cardMesa-buttons">
-          <button class="config-button">
-            <i class="fa-solid fa-gear"></i>
-          </button>
-          <button class="delete-button">
-            <i class="fa-solid fa-xmark"></i>
-          </button>
+    novaMesa.innerHTML = `
+      <div onclick="window.location.href='table.html'">
+        <h3 class="cardMesa-titulo">Nome da Mesa</h3>
+        <div class="cardMesa-header">
+          <img src="../media/logo_mesa.png" alt="Imagem do Card" class="cardMesa-img" />
+          <div class="cardMesa-buttons">
+            <button class="config-button">
+              <i class="fa-solid fa-gear"></i>
+            </button>
+            <button class="delete-button">
+              <i class="fa-solid fa-xmark"></i>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  ;
+    `;
 
     // Adiciona a nova mesa à lista de mesas com o ID "lista_mesas".
     document.getElementById('lista_mesas').appendChild(novaMesa);
